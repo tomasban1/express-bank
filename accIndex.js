8e23556c435f6889ce1a5316fe11017bb956d77f
tomasban1 <tomasbanius@gmail.com>
2024-08-31 01:48:14 +0300

    validations

---
+++ accIndex.js
@@ -0,0 +1,5 @@
+// import { accountList } from ".";
+
+// export function accountIndex(){
+//     accountList.map((acc, index) => )
+// }
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/api', (req, res) => {
     return res.json(data);
 });
 
-const accountList = [];
+export const accountList = [];
 
 app.get('/api/account', (req, res) => {
     return res.json(accountList)
@@ -54,10 +54,28 @@ app.post('/api/account', (req, res) => {
         });
     }
 
+    for (const person of accountList) {
+
+        if (person.name === name
+            && person.surname === surname
+            && person.dateOfBirth === req.body.dateOfBirth) {
+            return res.json({
+                state: 'Error',
+                message: 'Account with the same data already exists.',
+            });
+        }
+    }
+
+    const minLength = 2;
+    const maxLength = 20;
+
+    console.log(req.body);
+
     accountList.push({
         name: req.body.name,
         surname: req.body.surname,
         dateOfBirth: req.body.dateOfBirth,
+        accMoneyBalance: req.body.accMoneyBalance
     });
 
     return res.json({
@@ -79,6 +97,64 @@ app.get('/api/account/:name-:surname', (req, res) => {
         state: 'Error',
         message: 'Such person does not exist.',
     });
+
+});
+
+app.delete('/api/account/:name-:surname', (req, res) => {
+    const name = req.params.name.toLowerCase();
+    const surName = req.params.surname.toLowerCase();
+    for (const person of accountList) {
+        if (person.accMoneyBalance > 0) {
+            return res.json({
+                state: 'Error',
+                message: 'Money balance has to be 0 to delete account',
+            });
+        }
+        if (person.name.toLowerCase() === name && person.surname.toLowerCase() === surName) {
+            accountList.splice(person, 1);
+            return res.json({
+                state: 'Success',
+                message: 'Account successfully deleted',
+            });
+        }
+    }
+    return res.json({
+        state: 'Error',
+        message: 'No account with such person.',
+    });
+});
+
+app.put('/api/account/:name-:surname', (req, res) => {
+    const newName = req.query.newName;
+    const newSurname = req.query.newSurname;
+    const newDOB = req.query.newDateOfBirth;
+
+    for (const person of accountList) {
+        person.name === newName;
+        person.surname === newSurname;
+        person.dateOfBirth === newDOB;
+    }
+
+    return res.json({
+        state: 'success',
+        message: 'Person name successfuly updated.'
+    })
+
+});
+
+app.get('/api/account/:name-:surname/:personName', (req, res) => {
+    const name = req.params.personName.toLowerCase();
+    if (typeof name !== 'string') {
+        return res.json({
+            state: 'error',
+            message: 'Person name has to be text.'
+        })
+    }
+    accountList.map(acc => acc.name.toLowerCase() === name ? res.send(name) : res.json({
+        state: 'error',
+        message: 'Person by that name dosent exist'
+    }));
+
 
 });
 
